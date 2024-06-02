2c9b1c98fcf9c77a909e656db0c89d5716837aca
Victor Barreto <victorbarretolins@gmail.com>
2024-06-02 20:28:10 -0300

    chore: adicionado esqueleto para funcionalidade de deletar e detalhes

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:teste/models/filme.dart';
@@ -15,7 +13,8 @@ class _HomeScreenState extends State<HomeScreen> {
   var _filmesService = FilmeService();
   List<Filme> _filmesList = [];
 
-  void initState(){
+  @override
+  void initState() {
     super.initState();
     getAllFilmes();
   }
@@ -30,76 +29,100 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  _deleteFilme(int? id) async {
+    if(id == null) return;
+    print("deletando item");
+    print(id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-            "Catalogo Filmes",
-            style: TextStyle(color: Colors.white),
+          "Catalogo Filmes",
+          style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
       ),
       body: Padding(
         padding: EdgeInsets.only(top: 12, left: 16, right: 16, bottom: 16),
         child: ListView.builder(
-            itemCount: _filmesList.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(8.0),
-                  title: Row(
-                    children: [
-                      Image.network(
-                        _filmesList[index].urlFilme,
-                        width: 100,
-                        height: 200,
-                        fit: BoxFit.cover,
-                        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-                          return const Text('Invalid URL');
-                        },
-                      ),
-                      SizedBox(width: 16), // Espaçamento entre a imagem e a coluna
-                      // Coluna à direita com titulo, genero e faixaEtaria
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              _filmesList[index].titulo,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(height: 6), // Espaçamento entre os textos
-                            Text(
-                              'Gênero: ${_filmesList[index].genero}',
-                              style: TextStyle(
-                                fontSize: 14,
-                              ),
-                            ),
-                            SizedBox(height: 6), // Espaçamento entre os textos
-                            Text(
-                              'Faixa Etária: ${_filmesList[index].faixaEtaria}',
-                              style: TextStyle(
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
+          itemCount: _filmesList.length,
+          itemBuilder: (context, index) {
+            final filme = _filmesList[index];
+            return Dismissible(
+              key: Key(filme.id.toString()),
+              direction: DismissDirection.endToStart,
+              onDismissed: (direction) {
+                _deleteFilme(filme.id?.toInt());
+              },
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(Icons.delete, color: Colors.white),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  print('CLICADO EM ID: ${filme.id}');
+                },
+                child: Card(
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(8.0),
+                    title: Row(
+                      children: [
+                        Image.network(
+                          filme.urlFilme,
+                          width: 100,
+                          height: 200,
+                          fit: BoxFit.cover,
+                          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                            return const Text('Invalid URL');
+                          },
                         ),
-                      ),
-                    ],
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                filme.titulo,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 6),
+                              Text(
+                                'Gênero: ${filme.genero}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                              SizedBox(height: 6),
+                              Text(
+                                'Faixa Etária: ${filme.faixaEtaria}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              );
-            }
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
           Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => CadastrarFilme()))
+              .push(MaterialPageRoute(builder: (context) => CadastrarFilme()))
         },
         child: Icon(Icons.add),
       ),
