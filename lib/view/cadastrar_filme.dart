ab6f70f143c3ce057aa5ffb339decaa6958898b7
Victor Barreto <victorbarretolins@gmail.com>
2024-06-02 22:06:25 -0300

    chore: validando campos para cadastrar filme

--- lib/view/cadastrar_filme.dart
+++ lib/view/cadastrar_filme.dart
@@ -10,6 +10,7 @@ class CadastrarFilme extends StatefulWidget {
 }
 
 class _CadastrarFilmeState extends State<CadastrarFilme> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _urlDaImagem = TextEditingController();
   final TextEditingController _tituloController = TextEditingController();
   final TextEditingController _generoController = TextEditingController();
@@ -33,172 +34,209 @@ class _CadastrarFilmeState extends State<CadastrarFilme> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _urlDaImagem,
-              decoration: InputDecoration(
-                labelText: 'URL da Imagem',
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _tituloController,
-              decoration: InputDecoration(
-                labelText: 'Título',
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _generoController,
-              decoration: InputDecoration(
-                labelText: 'Gênero',
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _duracaoController,
-              decoration: InputDecoration(
-                labelText: 'Duração',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-            ),
-
-            SizedBox(height: 16.0),
-            Row(
-              children: [
-                Text("Faixa etária"),
-                SizedBox(width: 16.0), // Espaço de 16 pixels
-                Expanded(
-                  child: DropdownButton<String>(
-                    value: _selectedClassificacao,
-                    isExpanded: true, // Adiciona esta linha para expandir o dropdown
-                    items: <String>['Livre', '10', '12', '14', '16', '18']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedClassificacao = newValue!;
-                      });
-                    },
-                  ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _urlDaImagem,
+                decoration: InputDecoration(
+                  labelText: 'URL da Imagem',
+                  border: OutlineInputBorder(),
                 ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              children: [
-                Text("Nota: "),
-                Expanded(
-                  child: RatingBar.builder(
-                    initialRating: _rating,
-                    minRating: 1,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                    itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _tituloController,
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _generoController,
+                decoration: InputDecoration(
+                  labelText: 'Gênero',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _duracaoController,
+                decoration: InputDecoration(
+                  labelText: 'Duração',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Text("Faixa etária"),
+                  SizedBox(width: 16.0), // Espaço de 16 pixels
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      value: _selectedClassificacao,
+                      isExpanded: true, // Adiciona esta linha para expandir o dropdown
+                      items: <String>['Livre', '10', '12', '14', '16', '18']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedClassificacao = newValue!;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
                     ),
-                    onRatingUpdate: (rating) {
-                      setState(() {
-                        _rating = rating;
-                      });
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Text("Nota: "),
+                  Expanded(
+                    child: RatingBar.builder(
+                      initialRating: _rating,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                        setState(() {
+                          _rating = rating;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _anoController,
+                decoration: InputDecoration(
+                  labelText: 'Ano',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _descricaoController,
+                decoration: InputDecoration(
+                  labelText: 'Descrição',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
                     },
+                    child: Text('Cancelar'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red, // Cor vermelha para o botão de cancelar
+                    ),
                   ),
-                ),
-              ],
-            ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        var filme = Filme(
+                          titulo: _tituloController.text,
+                          urlFilme: _urlDaImagem.text,
+                          genero: _generoController.text,
+                          faixaEtaria: _selectedClassificacao,
+                          //rating: _rating,
+                          // ano: _selectedAno ?? _anoController.text,
+                        );
 
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _anoController,
-              decoration: InputDecoration(
-                labelText: 'Ano',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-            ),
-
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _descricaoController,
-              decoration: InputDecoration(
-                labelText: 'Descricao',
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Cancelar'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red, // Cor vermelha para o botão de cancelar
+                        var result = await _filmeService.salvarFilme(filme);
+                        if (result != null) {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("Sucesso"),
+                                content: Text("O filme foi criado com sucesso!"),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text("OK"),
+                                  ),
+                                ],
+                              );
+                            },
+                          ).then((value) {
+                            Navigator.of(context).pop(); // Dar pop na tela
+                          });
+                        }
+                      }
+                    },
+                    child: Text('Cadastrar'),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    var filme = Filme(
-                      titulo: _tituloController.text,
-                      urlFilme: _urlDaImagem.text,
-                      genero: _generoController.text,
-                      faixaEtaria: _selectedClassificacao,
-                     //rating: _rating,
-                     // ano: _selectedAno ?? _anoController.text,
-                    );
-
-                    var result = await _filmeService.salvarFilme(filme);
-                    if (result != null) {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Sucesso"),
-                            content: Text("O filme foi criado com sucesso!"),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: Text("OK"),
-                              ),
-                            ],
-                          );
-                        },
-                      ).then((value) {
-                        Navigator.of(context).pop(); // Dar pop na tela
-                      });
-                    }
-                  },
-                  child: Text('Cadastrar'),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
