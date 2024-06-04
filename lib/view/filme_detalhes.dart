e295315e2e1b0610f01f3b8135efe5b3c8dd5791
Victor Barreto <victorbarretolins@gmail.com>
2024-06-04 13:13:09 -0300

    chore: finalizado layout da area de detalhes

--- lib/view/filme_detalhes.dart
+++ lib/view/filme_detalhes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:teste/models/filme.dart';
 import 'package:teste/services/filme_service.dart';
 
@@ -52,26 +53,81 @@ class _FilmeDetalhesState extends State<FilmeDetalhes> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(
-              _filme.urlFilme,
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) => Text('Invalid URL'),
+            Align(
+              alignment: Alignment.center,
+              child: Image.network(
+                _filme.urlFilme,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                    Text('Invalid URL'),
+              ),
             ),
             SizedBox(height: 16),
-            Text(
-              _filme.titulo,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _filme.titulo,
+                  style: TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                Text(_filme.ano,
+                    style: TextStyle(fontSize: 18)),
+              ],
+            ),
+            SizedBox(height: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _filme.genero,
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight:
+                      FontWeight.w400,
+                      color: Colors.grey
+                  ),
+                ),
+                Text(
+                  _filme.faixaEtaria,
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight:
+                      FontWeight.w400,
+                      color: Colors.grey
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _filme.duracao,
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight:
+                      FontWeight.w400,
+                      color: Colors.grey
+                  ),
+                ),
+                RatingBarIndicator(
+                  rating: _filme.rating,
+                  itemBuilder: (context, index) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  itemCount: 5,
+                  itemSize: 20.0,
+                  direction: Axis.horizontal,
+                ),
+              ],
             ),
             SizedBox(height: 8),
-            Text('Gênero: ${_filme.genero}', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 8),
-            Text('Faixa Etária: ${_filme.faixaEtaria}', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 8),
-            Text('Ano: ${_filme.ano}', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 8),
-            Text('Nota: ${_filme.rating}', style: TextStyle(fontSize: 18)),
+            Text(_filme.descricao,
+                style: TextStyle(fontSize: 18)),
           ],
         ),
       ),
